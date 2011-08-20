4e56f6bc3eaa0ddbb88c2d7e5b1dfcca70150b2b
h13i32maru <h13i32maru@gmail.com>
2011-08-20 14:30:46 +0900

    ウィジェット対応

--- gen/jp/h13i32maru/calorie/R.java
+++ gen/jp/h13i32maru/calorie/R.java
@@ -24,31 +24,36 @@ containing a value of this type.
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int caloire_icon=0x7f060008;
-        public static final int calorie_name=0x7f060009;
-        public static final int calorie_value=0x7f06000a;
-        public static final int dec_button=0x7f060003;
-        public static final int dec_slow_button=0x7f060004;
-        public static final int inc_button=0x7f060006;
-        public static final int inc_slow_button=0x7f060005;
-        public static final int limit=0x7f060000;
-        public static final int max=0x7f060001;
-        public static final int multi_bar=0x7f060002;
-        public static final int table_calorie_info=0x7f060007;
-        public static final int total_calorie_value=0x7f06000b;
+        public static final int bar_image=0x7f07000c;
+        public static final int caloire_icon=0x7f070008;
+        public static final int calorie_name=0x7f070009;
+        public static final int calorie_value=0x7f07000a;
+        public static final int dec_button=0x7f070003;
+        public static final int dec_slow_button=0x7f070004;
+        public static final int inc_button=0x7f070006;
+        public static final int inc_slow_button=0x7f070005;
+        public static final int limit=0x7f070000;
+        public static final int max=0x7f070001;
+        public static final int multi_bar=0x7f070002;
+        public static final int table_calorie_info=0x7f070007;
+        public static final int total_calorie_value=0x7f07000b;
     }
     public static final class layout {
         public static final int config=0x7f030000;
         public static final int main=0x7f030001;
         public static final int table_row_calorie_info=0x7f030002;
         public static final int table_row_total=0x7f030003;
+        public static final int widget=0x7f030004;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
     }
     public static final class style {
-        public static final int BarButton=0x7f050000;
+        public static final int BarButton=0x7f060000;
+    }
+    public static final class xml {
+        public static final int widget=0x7f040000;
     }
     public static final class styleable {
         /** Attributes that can be used with a MultiBar.
